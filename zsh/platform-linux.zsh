48c4f98f11885cdd85abebceea231cb865616dcf
James Simonsen <james@glean.com>
2026-03-10 13:49:59 -0700

    Set LS_COLORS on Linux to match Mac color scheme
    Executables show as blue bold (not green), directories as cyan,
    and no green background on sticky/other-writable directories.

--- zsh/platform-linux.zsh
+++ zsh/platform-linux.zsh
@@ -2,6 +2,10 @@ path+=("$HOME/bin")
 path+=("$HOME/.local/bin")
 path+=("$HOME/go/bin")
 
+# LS colors matching Mac LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
+# Directories: cyan, symlinks: magenta, executables: blue bold
+# Disable green background on sticky/other-writable dirs
+export LS_COLORS='di=36:ln=35:so=31;1:pi=34;1:ex=34;1:bd=0:cd=0:su=37;1:sg=0:tw=36:ow=36'
 alias ls='ls --color=auto'
 alias ll='ls -l --color=auto'
 
